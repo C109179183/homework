1d5ef1c0152338deb7fd1d4ec1e7dc14c466b644
C109179183 <102135639+C109179183@users.noreply.github.com>
2022-06-30 18:19:54 +0800

    Add files via upload

--- 3.py
+++ 3.py
@@ -1,28 +1,25 @@
-s = int(input("請輸入度數"))
-b = 0
-c = 0
-if a <= 120:
-    b = a * 2.10
-    c = a * 2.10
-    print("summer months:",b)
-    print("Non-summer months:",c)
-elif a > 120 and a <= 330:
-    b = ((a - 120) * 3.02) + 252
-    c = ((a - 120) * 2.68) + 252
-    print("summer months:",b)
-    print("Non-summer months:",c)
-elif a > 330 and a <= 500:
-    b = ((a - 330) * 4.39) + 252 + 634.2
-    c = ((a - 330) * 3.61) + 252 + 562.8
-    print("summer months:",b)
-    print("Non-summer months:",c)
-elif a > 500 and a <= 700:
-    b = ((a - 500) * 4.97) + 252 + 634.2 + 746.3
-    c = ((a - 500) * 4.01) + 252 + 562.8 + 613.7
-    print("summer months:",b)
-    print("Non-summer months:",c)
-elif a > 700:
-    b = ((a - 700) * 5.63) + 252 + 634.2 + 746.3 + 994
-    c = ((a - 700) * 4.50) + 252 + 562.8 + 613.7 + 802
-    print("summer months:",b)
-    print("Non-summer months:",c)
+a = int(input ("請輸入西元年:"))
+if a%12 == 0 :
+    print("monkey")
+elif a%12 == 1:
+    print("rooster")
+elif a%12 == 2:
+    print("dog")
+elif a%12 == 3:
+    print("pig")
+elif a%12 == 4:
+    print("rat")
+elif a%12 == 5:
+    print("ox")
+elif a%12 == 6:
+    print("tiger")
+elif a%12 == 7:
+    print("rabbit")
+elif a%12 == 8:
+    print("dragon")
+elif a%12 == 9:
+    print("snake")
+elif a%12 == 10:
+    print("horse")
+elif a%12 == 11:
+    print("sheep")
